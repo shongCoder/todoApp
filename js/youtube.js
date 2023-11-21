b334001038b93da62d13149c19fa09fafff12d3b
Shong <120701765+shongCoder@users.noreply.github.com>
2023-11-21 14:03:57 +0900

    Update youtube.js

--- js/youtube.js
+++ js/youtube.js
@@ -3,9 +3,9 @@ const youTubes = [
     // 신이 되는 법
     `<iframe src="https://www.youtube.com/embed/Vur832hIn0k?si=Xe5Kr_Ap4ltYxQUK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
     // 우주의 빈 공간
-    `<iframe width="560" height="315" src="https://www.youtube.com/embed/4Io1dr6aYl8?si=fCqbdSyYemFLbBAl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
+    `<iframe src="https://www.youtube.com/embed/4Io1dr6aYl8?si=fCqbdSyYemFLbBAl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
     // 바퀴벌레는 왜 인간을 못이길까
-    `<iframe width="560" height="315" src="https://www.youtube.com/embed/FhA37Sw4j8w?si=sWD_Wxr5VlCYjXUU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
+    `<iframe src="https://www.youtube.com/embed/FhA37Sw4j8w?si=sWD_Wxr5VlCYjXUU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
     // 고양이 힐링
 ];
 
